55d7d481f819a70460c1b154747a4f89b06bac05
Jakub-Golinowski <jakub.golinowski@gmail.com>
2018-06-02 17:38:57 +0100

    Clean up includes of to reduce compile time hpx_mandelbrot.cpp

--- hpx_mandelbrot/hpx_mandelbrot.cpp
+++ hpx_mandelbrot/hpx_mandelbrot.cpp
@@ -1,29 +1,16 @@
-#include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
 //
 #include <hpx/parallel/algorithms/for_loop.hpp>
 #include <hpx/parallel/execution.hpp>
 //
 #include <hpx/runtime/resource/partitioner.hpp>
-#include <hpx/runtime/threads/cpu_mask.hpp>
-#include <hpx/runtime/threads/detail/scheduled_thread_pool_impl.hpp>
-#include <hpx/runtime/threads/executors/pool_executor.hpp>
 //
 #include <hpx/include/iostreams.hpp>
-#include <hpx/include/runtime.hpp>
 //
 #include "system_characteristics.hpp"
 //
-#include <cmath>
-#include <cstddef>
 #include <iostream>
-#include <memory>
-#include <set>
-#include <stdexcept>
-#include <string>
-#include <utility>
 //
-#include <iostream>
 #include <opencv2/opencv.hpp>
 
 ///////////////////////////////////////////////////////////////////////////
