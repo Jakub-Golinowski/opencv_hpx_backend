4b632fa3ca4369f7b6a4d8bb2f3d5f3792f0b908
Jakub-Golinowski <jakub.golinowski@gmail.com>
2018-06-03 16:09:35 +0100

    Tidy up includes in hpx_load_image

--- hpx_image_load/hpx_image_load.cpp
+++ hpx_image_load/hpx_image_load.cpp
@@ -1,34 +1,16 @@
-//  Copyright (c) 2017 Shoshana Jakobovits
-//
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
 //
 #include <hpx/parallel/algorithms/for_loop.hpp>
 #include <hpx/parallel/execution.hpp>
 //
 #include <hpx/runtime/resource/partitioner.hpp>
-#include <hpx/runtime/threads/cpu_mask.hpp>
-#include <hpx/runtime/threads/detail/scheduled_thread_pool_impl.hpp>
-#include <hpx/runtime/threads/executors/pool_executor.hpp>
 //
 #include <hpx/include/iostreams.hpp>
-#include <hpx/include/runtime.hpp>
-//
-#include <cmath>
-#include <cstddef>
-#include <iostream>
-#include <memory>
-#include <set>
-#include <string>
-#include <utility>
-#include <stdexcept>
 //
 #include "system_characteristics.hpp"
 //
 #include <iostream>
+//
 #include <opencv2/opencv.hpp>
 
 
