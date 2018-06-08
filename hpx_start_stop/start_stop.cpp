b816946991fe12f43245c6fb05805b5c336a1b2b
Jakub-Golinowski <jakub.golinowski@gmail.com>
2018-06-08 14:43:04 +0200

    Add config to start_stop example

--- hpx_start_stop/start_stop.cpp
+++ hpx_start_stop/start_stop.cpp
@@ -10,17 +10,24 @@
 //
 #include <hpx/include/iostreams.hpp>
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
-
     int my_argc = 1;
-    char str[] = "backend_launch";
-    char* str_ptr = str;
+    char app[] = "backend_launch";
+    char* app_ptr = app;
+    char * my_argv[] = {app_ptr, nullptr};
 
-    char * my_argv[] = {str_ptr};
+    int num_cores = 6;
+    std::string cfg_string;
 
-    hpx::start(nullptr, my_argc, my_argv);
+    cfg_string =  "hpx.os_threads=" + std::to_string(num_cores);
+    std::vector<std::string> const cfg = {
+            cfg_string
+    };
+
+    hpx::start(nullptr, my_argc, my_argv, cfg);
 
     // Wait for runtime to start
     hpx::runtime* rt = hpx::get_runtime_ptr();
@@ -30,6 +37,12 @@ int main(int argc, char* argv[])
     int counter = 0;
     hpx::lcos::local::mutex m;
 
+    std::size_t num_work_threads = -1;
+
+    hpx::apply( [&]() {
+        num_work_threads = hpx::get_num_worker_threads();
+    });
+
     hpx::apply( [&]() {
         hpx::parallel::for_loop(
             hpx::parallel::execution::par,
@@ -37,6 +50,9 @@ int main(int argc, char* argv[])
             [&](std::size_t i) {
                 std::lock_guard<hpx::lcos::local::mutex> lock(m);
                 ++counter;
+
+                hpx::get_num_worker_threads();
+
                 hpx::cout << std::hex << hpx::this_thread::get_id() << " "
                           << std::hex << std::this_thread::get_id()
                           << " i = " << std::dec << i << std::endl;
@@ -47,7 +63,9 @@ int main(int argc, char* argv[])
     hpx::apply([]() { hpx::finalize(); });
     hpx::stop();
 
-    std::cout << "Counter: " << counter;
+    std::cout << "Counter: " << counter << std::endl;
+    std::cout << "Num worker threads: " << num_work_threads
+              << " (expected: " + std::to_string(num_cores) + ")";
 
     return 0;
 }
