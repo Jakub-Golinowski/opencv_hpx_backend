20e44cc7c557cb098d01d9084c9a4e050cccf97b
Jakub-Golinowski <jakub.golinowski@gmail.com>
2018-06-10 15:37:34 +0200

    Extend the mandelbrot_benchmar.py to cover sweep over pus, saving plots to images and pdf

--- python/mandelbrot_benchmark/mandelbrot_benchmark.py
+++ python/mandelbrot_benchmark/mandelbrot_benchmark.py
@@ -2,12 +2,20 @@ import numpy as np
 import matplotlib.pylab as plt
 import argparse
 from enum import Enum
+import os
+from matplotlib.ticker import EngFormatter
+from matplotlib.backends.backend_pdf import PdfPages
 
 
 class Metric(Enum):
     PARALLEL_TIME = 0
     SEQUENTIAL_TIME = 1
     SPEEDUP = 2
+
+
+class Sweep(Enum):
+    SIZE = 0
+    NUM_PUS = 1
 
 
 def plot_backends_over_key(backend_names, backends_perf_dict,
@@ -18,7 +26,6 @@ def plot_backends_over_key(backend_names, backends_perf_dict,
 
     if isinstance(metric, Metric):
         m = metric.value
-        print("Using metric: " + str(Metric(metric)) )
     else:
         print("ERROR: metric is not instance of class Metric: " + str(metric))
         exit(-1)
@@ -36,16 +43,25 @@ def plot_backends_over_key(backend_names, backends_perf_dict,
                      fmt='o-', capsize=3, elinewidth=1,
                      markeredgewidth=1, label=backend_name)
 
+    ax.xaxis.set_major_formatter(EngFormatter())
+
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend()
     plt.grid(True)
 
+    if os.path.isdir(save_path):
+        file_name=title.replace(" ", "_")
+        full_save_path = os.path.join(save_path, file_name)
+        print("Saving image to path: " + full_save_path)
+        fig.savefig(full_save_path, dpi=300,
+                    format='png', bbox_inches='tight')
+
     return fig
 
 
-def parse_entry(lines, offset):
+def parse_entry(lines, offset, sweep=Sweep.SIZE):
     # lines - list of lines from log file
     # offset - offset in the logfiles
     #each entry is assumed to have 4 lines
@@ -53,83 +69,169 @@ def parse_entry(lines, offset):
     size_line_tokes = lines[0 + offset].split(" ")
     height = int(size_line_tokes[1].split("=")[1])
     width = int(size_line_tokes[2].split("=")[1])
+    num_pus = int(size_line_tokes[4].split("=")[1])
 
     parallel_time = float(lines[0 + offset + 1].split(" ")[2])
     sequential_time = float(lines[0 + offset + 2].split(" ")[2])
     speedup = float(lines[0 + offset + 3].split(" ")[1])
 
-    return [height*width, parallel_time, sequential_time, speedup]
+    if sweep == Sweep.SIZE:
+        return [height*width, parallel_time, sequential_time, speedup]
+    else:
+        return [num_pus, parallel_time, sequential_time, speedup]
 
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("logs_path",
-                        help="Path to the folder with run logs")
-    args = parser.parse_args()
-    logs_path = args.logs_path
-
+def extract_perf(backend_names, sweep, ):
     backends_dict = {}
-    backends_devs_dict = {}
+    backends_std_dict = {}
 
-    # backend_names = ["hpx", "hpx_startstop", "tbb"]
-    backend_names = ["hpx", "hpx_startstop", "tbb"]
     for backend_name in backend_names:
-
         # Each entry of dict is a list of:
-        # #0 parallel time
-        # #1 sequential time
-        # #2 speedup
+        # 0 parallel time
+        # 1 sequential time
+        # 2 speedup
         backend_perf_dict = {}
-        logfile = logs_path + "/" + backend_name + "-mandelbrot_over_workload.log"
+        backend_std_perf_dict = {}
+        logfile=""
+        if sweep == Sweep.SIZE:
+            logfile = logs_path + "/" + backend_name + \
+                      "-mandelbrot_over_workload.log"
+        elif sweep == Sweep.NUM_PUS:
+            logfile = logs_path + "/" + backend_name + \
+                      "-mandelbrot_over_num_pus.log"
+        else:
+            print("ERROR: Wrong key: " + str(sweep))
+            exit(-1)
 
         with open(logfile) as file:
             lines = file.read().splitlines()
 
             num_entries = round(len(lines)/4)
             for i in range(0, num_entries):
-                entry = parse_entry(lines, 4 * i)
+                entry = parse_entry(lines, 4 * i, sweep)
                 key = entry[0]
                 value = entry[1:]
                 if key in backend_perf_dict:
-                    # backend_perf_dict[key] = np.average([backend_perf_dict[key], value], axis=0)
                     backend_perf_dict[key].append(value)
                 else:
                     backend_perf_dict[key] = [value]
 
-        backend_perf_devs_dict = {}
+
         for key in backend_perf_dict.keys():
-            averges = \
+            averages = \
                     [float(sum(col)) / len(col) for col
-                                                in zip(*backend_perf_dict[key])]
+                     in zip(*backend_perf_dict[key])]
             stds = [np.std(np.asarray(col), axis=0)
                     for col in zip(*backend_perf_dict[key])]
-            backend_perf_dict[key] = averges
-            backend_perf_devs_dict[key] = stds
+            backend_perf_dict[key] = averages
+            backend_std_perf_dict[key] = stds
 
         backends_dict[backend_name] = backend_perf_dict
-        backends_devs_dict[backend_name] = backend_perf_devs_dict
+        backends_std_dict[backend_name] = backend_std_perf_dict
 
-    fig = plot_backends_over_key(backend_names, backends_dict,
-                           backends_devs_dict, Metric.PARALLEL_TIME,
-                           title="Parallel processing time as function of "
+    return backends_dict, backends_std_dict
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("logs_path",
+                        help="Path to the folder with run logs")
+    parser.add_argument("-i", "--im_save_path",
+                        help="path in which images will be saved")
+    parser.add_argument("-p", "--pdf_save_path",
+                        help="path in pdf will be saved")
+    args = parser.parse_args()
+
+    logs_path = args.logs_path
+    im_save_path = args.im_save_path if args.im_save_path else ""
+    pdf_save_path = args.pdf_save_path if args.pdf_save_path else ""
+    pp = None
+    if os.path.isdir(pdf_save_path):
+        f_name = "mandelbrot_benchmark.pdf"
+        pp = PdfPages(os.path.join(pdf_save_path, f_name))
+
+    # =========================================================================
+    #                      SWEEP OVER THE WORKLOAD SIZE
+    # =========================================================================
+    # backend_names = ["hpx", "hpx_startstop", "hpx_nstripes",
+    #                  "hpx_nstripes_startstop", "tbb", "pthreads"]
+    backend_names = ["hpx", "hpx_startstop", "tbb", "pthreads"]
+    backends_size_dict, backends_size_devs_dict = extract_perf(backend_names,
+                                                               Sweep.SIZE)
+
+
+    fig = plot_backends_over_key(backend_names, backends_size_dict,
+                                 backends_size_devs_dict, Metric.PARALLEL_TIME,
+                                 title="Parallel processing time as function of "
                                  "image size",
-                           xlabel="Number of pixels",
-                           ylabel="Processing time [s]")
+                                 xlabel="Number of pixels",
+                                 ylabel="Processing time [s]",
+                                 save_path=im_save_path)
+    if os.path.isdir(pdf_save_path):
+        pp.savefig(fig)
     plt.show()
 
-    fig = plot_backends_over_key(backend_names, backends_dict,
-                           backends_devs_dict, Metric.SEQUENTIAL_TIME,
-                           title="Sequential processing time as function of "
+    fig = plot_backends_over_key(backend_names, backends_size_dict,
+                                 backends_size_devs_dict, Metric.SEQUENTIAL_TIME,
+                                 title="Sequential processing time as function of "
                                  "image size",
-                           xlabel="Number of pixels",
-                           ylabel="Processing time [s]")
+                                 xlabel="Number of pixels",
+                                 ylabel="Processing time [s]",
+                                 save_path=im_save_path)
+    if os.path.isdir(pdf_save_path):
+        pp.savefig(fig)
     plt.show()
 
-    fig = plot_backends_over_key(backend_names, backends_dict,
-                           backends_devs_dict, Metric.SPEEDUP,
-                           title="Speed-up as function of "
+    fig = plot_backends_over_key(backend_names, backends_size_dict,
+                                 backends_size_devs_dict, Metric.SPEEDUP,
+                                 title="Speed-up as function of "
                                  "image size",
-                           xlabel="Number of pixels",
-                           ylabel="Speed-up")
+                                 xlabel="Number of pixels",
+                                 ylabel="Speed-up",
+                                 save_path=im_save_path)
+    if os.path.isdir(pdf_save_path):
+        pp.savefig(fig)
     plt.show()
+
+    # =========================================================================
+    #               SWEEP OVER THE NUMBER OF Processing Units (PUs)
+    # =========================================================================
+
+    backend_names = ["hpx", "hpx_startstop"]
+    backends_pus_dict, backends_pus_std_dict = extract_perf(backend_names,
+                                                            Sweep.NUM_PUS)
+
+    fig = plot_backends_over_key(backend_names, backends_pus_dict,
+                                 backends_pus_std_dict, Metric.PARALLEL_TIME,
+                                 title="Parallel processing time as function of "
+                                 "number of PUs",
+                                 xlabel="Number of processing units (PUs)",
+                                 ylabel="Processing time [s]",
+                                 save_path=im_save_path)
+    if os.path.isdir(pdf_save_path):
+        pp.savefig(fig)
+    plt.show()
+
+    fig = plot_backends_over_key(backend_names, backends_pus_dict,
+                                 backends_pus_std_dict, Metric.SEQUENTIAL_TIME,
+                                 title="Sequential processing time as function of "
+                                 "number of PUs",
+                                 xlabel="Number of processing units (PUs)",
+                                 ylabel="Processing time [s]",
+                                 save_path=im_save_path)
+    if os.path.isdir(pdf_save_path):
+        pp.savefig(fig)
+    plt.show()
+
+    fig = plot_backends_over_key(backend_names, backends_pus_dict,
+                                 backends_pus_std_dict, Metric.SPEEDUP,
+                                 title="Speed-up as function of "
+                                 "number of PUs",
+                                 xlabel="Number of processing units (PUs)",
+                                 ylabel="Speed-up",
+                                 save_path=im_save_path)
+    if os.path.isdir(pdf_save_path):
+        pp.savefig(fig)
+    plt.show()
+
+    pp.close()
