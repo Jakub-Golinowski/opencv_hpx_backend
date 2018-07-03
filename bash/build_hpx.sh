6a9ec66f70af288a4bcb57943761081deba3ae18
Jakub-Golinowski <jakub.golinowski@gmail.com>
2018-07-03 15:43:17 +0100

    Change tabs to 4 spaces and fix sha-bang

--- bash/build_hpx.sh
+++ bash/build_hpx.sh
@@ -1,14 +1,14 @@
-#/bin/bash
+#!/bin/bash
 
 # this script is used to build HPX library with different options
 
 ### ===========================================================
-### 						INCLUDES
+###                         INCLUDES
 ### ===========================================================
 # source ./common_functions.sh
 
 ### ===========================================================
-###						INPUT PARAMETERS
+###                        INPUT PARAMETERS
 ### ===========================================================
 # Default parameter values
 MESSAGE="-- no message provided --"
@@ -24,57 +24,57 @@ key="$1"
 
 case $key in
     -mes|--message)
-	    MESSAGE="$2"
-	    shift # past argument
-	    shift # past value
+        MESSAGE="$2"
+        shift # past argument
+        shift # past value
     ;;
-	-hp|--hpx-path)
-		HPX_PATH="$2"
-		shift; shift
-	;;
-	-lp|--logs-path)
-		LOGS_PATH="$2"
-		shift; shift
-	;;
+    -hp|--hpx-path)
+        HPX_PATH="$2"
+        shift; shift
+    ;;
+    -lp|--logs-path)
+        LOGS_PATH="$2"
+        shift; shift
+    ;;
     *)    # unknown option
-	    POSITIONAL+=("$1") # save it in an array for later
-	    shift # past argument
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
     ;;
 esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 ### ===========================================================
-###         			FUNCTIONS
+###                     FUNCTIONS
 ### ===========================================================
 function build_hpx {
-	local mode=$1
+    local mode=$1
 
-	echo -e "\n===== BUILDING HPX in ${mode} mode ====="
+    echo -e "\n===== BUILDING HPX in ${mode} mode ====="
 
-	echo -e "\nGoing to ${HPX_PATH}build"
-	cd ${OPENCV_PATH}build
-	echo -e "\nContents of the `pwd`"
-	ls
+    echo -e "\nGoing to ${HPX_PATH}build"
+    cd ${OPENCV_PATH}build
+    echo -e "\nContents of the `pwd`"
+    ls
 
-	echo -e "\nCreating the ${mode} directory"
-	mkdir ${mode}
-	echo -e "\nEntering the ${mode} directory"
-	cd ${mode}
-	echo -e "\nRunning cmake"
+    echo -e "\nCreating the ${mode} directory"
+    mkdir ${mode}
+    echo -e "\nEntering the ${mode} directory"
+    cd ${mode}
+    echo -e "\nRunning cmake"
 
-	if [ ${mode} = "debug" ]; then
-		build_type="Debug"
-	elif [ ${mode} = "release"]; then
-    	build_type=Release
-	else
-		echo "ERROR: WRONG mode = ${mode}"
-		exit 1
-	fi
+    if [ ${mode} = "debug" ]; then
+        build_type="Debug"
+    elif [ ${mode} = "release"]; then
+        build_type=Release
+    else
+        echo "ERROR: WRONG mode = ${mode}"
+        exit 1
+    fi
 
-	echo "Build type: ${build_type}"
+    echo "Build type: ${build_type}"
 
-	cmake -DCMAKE_BUILD_TYPE=${build_type} \
+    cmake -DCMAKE_BUILD_TYPE=${build_type} \
       -DCMAKE_INSTALL_PREFIX=/home/jakub/packages/hpx \
       -DBOOST_ROOT=/home/jakub/packages/boost/boost_1_65_1 \
       -DHWLOC_ROOT=/home/jakub/packages/hwloc \
@@ -82,24 +82,24 @@ function build_hpx {
       -DHPX_WITH_CXX11=On \
       ../../
 
-	echo -e "\nBuilding OpenCV"      
+    echo -e "\nBuilding OpenCV"
 
-	make -j7
+    make -j7
 
-	echo "++++++++++++++++++++++++++++++++++"
+    echo "++++++++++++++++++++++++++++++++++"
 }
 ### ===========================================================
-### 						MAIN
+###                         MAIN
 ### ===========================================================
 
 TIMESTAMP=$(date +"%Y-%m-%d-%H.%M")
 
 echo "Executing script: ${0}"
 echo "Used parameters:"
-echo "	MESSAGE = ${MESSAGE}"
-echo "	LOGS_PATH = ${LOGS_PATH}"
-echo "	HPX_PATH = ${HPX_PATH}"
-echo "	REPO_ROOT_PATH = ${REPO_ROOT_PATH}"
+echo "    MESSAGE = ${MESSAGE}"
+echo "    LOGS_PATH = ${LOGS_PATH}"
+echo "    HPX_PATH = ${HPX_PATH}"
+echo "    REPO_ROOT_PATH = ${REPO_ROOT_PATH}"
 
 build_hpx "debug"
 build_hpx "release"
