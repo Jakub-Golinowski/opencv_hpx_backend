1a898852c0e4dbe5982e625aa1a7e5b45bebcf29
Jakub-Golinowski <jakub.golinowski@gmail.com>
2018-07-25 10:58:30 +0100

    Add qt_hpx_opencv example. It opens a main window in which user can choose the number of threads and then hit Run button. After that each thread takes image from the builtin webcam. On the GUI there is execution time of each thread counted from the job deployment.

---
+++ examples/qt_hpx_opencv/widget.hpp
@@ -0,0 +1,45 @@
+//  Copyright (c) 2012-2014 Thomas Heller
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <QDialog>
+
+#ifndef Q_MOC_RUN
+#include <hpx/include/threads.hpp>
+#include <hpx/lcos/local/spinlock.hpp>
+#include <cstddef>
+#include <functional>
+#endif
+
+class QWidget;
+class QListWidget;
+class QPushButton;
+
+class widget
+    : public QDialog
+{
+    Q_OBJECT
+
+    public:
+        widget(std::function<void(widget *, std::size_t)> callback,
+            QWidget *parent = nullptr);
+
+        void threadsafe_add_label(std::size_t i, double t);
+
+        void threadsafe_run_finished();
+
+    public slots:
+        void set_threads(int no);
+
+        void run_clicked(bool);
+
+        void add_label(const QString& text);
+
+    private:
+        std::size_t no_threads;
+        hpx::lcos::local::spinlock mutex;
+        QListWidget *list;
+        QPushButton * run_button;
+        std::function<void(widget *, std::size_t)> callback_;
+};
